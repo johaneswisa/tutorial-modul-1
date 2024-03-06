75b93bc5953253656054a607c626eef57f1eafc4
Johan <johaneswisanggeni2003@gmail.com>
2024-03-06 20:40:26 +0700

    [REFACTOR] refactor payment repository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,4 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import org.springframework.stereotype.Repository;
 public class PaymentRepository {
 }
