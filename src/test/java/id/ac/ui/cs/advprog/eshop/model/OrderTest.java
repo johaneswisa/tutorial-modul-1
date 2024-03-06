cbf6ed3135fd3521caba39db21f8199ece5db4d8
Johan <johaneswisanggeni2003@gmail.com>
2024-03-06 19:19:50 +0700

    [RED] Add OrderRepository skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -33,7 +33,7 @@ public class OrderTest {
         this.products.clear();
 
         assertThrows(IllegalArgumentException.class, () -> {
-            Order order = new Order("13652556-012a0-4c07-b546-54eb1396d79b",
+            Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
                     this.products, 1708560000L, "Safira Sudrajat");
         });
     }
